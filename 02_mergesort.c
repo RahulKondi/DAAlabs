ae2dcec07eb4ed6dc1d25d4aaf78bb3a6d03a56b
Rahul <kondirahul21@gmail.com>
2014-05-22 20:15:56 +0530

    test_push

--- 02_mergesort.c
+++ 02_mergesort.c
@@ -1,18 +1,19 @@
 /*
-01. DECALRE timespec VARIABLES
-02. GET NUMBER OF ELEMENTS (&n)
-03. CALL SRAND FUNCTION WITH ((unsigned)time(NULL)
-04. RANDOMISE ARRAY (a[i] = rand()%20) AND PRINT EACH ELEMENT GENERATED, IMMEDIATELY
+**main ()
+*01. DECALRE timespec VARIABLES
+*02. GET NUMBER OF ELEMENTS (&n)
+*03. CALL SRAND FUNCTION WITH ((unsigned)time(NULL)
+*04. RANDOMISE ARRAY (a[i] = rand()%20) AND PRINT EACH ELEMENT GENERATED, IMMEDIATELY
 
-05. START TIMER
+*05. START TIMER
 
-06. CALL sorting PROCEDURE WITH (0, n-1); (LOW , HIGH)
+*06. CALL sorting PROCEDURE WITH (0, n-1); (LOW , HIGH)
 
-07. STOP TIMER
-08. CALCULATE DIFFERENCE ( (time_STOP.tv_sec * 1000000000 + time_STOP.tv_nsec)  -  (time_START.tv_sec * 1000000000 + time_START.tv_nsec) ) 
+*07. STOP TIMER
+*08. CALCULATE DIFFERENCE ( (time_STOP.tv_sec * 1000000000 + time_STOP.tv_nsec)  -  (time_START.tv_sec * 1000000000 + time_START.tv_nsec) )
 
-09. PRINT SORTED ARRAY
-10. PRINT duration USING %e 
+*09. PRINT SORTED ARRAY
+*10. PRINT duration USING %e
 */
 
 
@@ -50,3 +51,6 @@ clock_gettime (CLOCK_REALTIME, &time_STOP);
 
  return 0;
 }
+
+void merge_sort (int LOW, int HIGH) {
+	
